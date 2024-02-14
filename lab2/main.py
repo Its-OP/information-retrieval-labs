d72e7ea42aa75246c7f7c9e58caa01dacfd36ff5
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-02-14 20:17:37 +0200

    Init lab

---
+++ lab2/main.py
@@ -0,0 +1,25 @@
+import os
+
+directory_path = input("Please enter the path to the directory: ")
+
+while True:
+    file_path = input("Please enter the path to the text file: ")
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+    terms = content.split(' ')
+
+    corpus = {}
+    for file_name in os.listdir(directory_path):
+        if file_name.endswith('.txt'):
+            file_path = os.path.join(directory_path, file_name)
+            with open(file_path, 'r') as file:
+                content = file.read()
+            corpus[file_name] = content
+
+    matching_documents = []
+    for name, content in corpus.items():
+        if any(word in content for word in terms):
+            matching_documents.append(name)
+
+    print(f'The following documents match the query: {matching_documents}')
