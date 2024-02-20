55f3033e3273a8cc4549d6faf69d188b3057f5b0
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-02-20 09:27:11 +0200

    Fix lab 1

--- lab1/main.py
+++ lab1/main.py
@@ -1,25 +1,28 @@
 import os
+import string
 
 directory_path = input("Please enter the path to the directory: ")
 corpus = {}
-
+translator = str.maketrans('', '', string.punctuation)
 for file_name in os.listdir(directory_path):
     if file_name.endswith('.txt'):
         file_path = os.path.join(directory_path, file_name)
         with open(file_path, 'r') as file:
             content = file.read()
-        corpus[file_name] = content
+        corpus[file_name] = set(content.translate(translator).split(' '))
 
 while True:
     file_path = input("Please enter the path to the text file: ")
 
+    or_terms = []
     with open(file_path, 'r') as file:
-        content = file.read()
-    terms = content.split(' ')
+        for line in file:
+            and_terms = line.strip().split(' ')
+            or_terms.append(and_terms)
 
     matching_documents = []
     for name, content in corpus.items():
-        if any(word in content for word in terms):
+        if any(all(and_term in content for and_term in and_terms) for and_terms in or_terms):
             matching_documents.append(name)
 
     print(f'The following documents match the query: {matching_documents}')
