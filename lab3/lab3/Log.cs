9a3d675ccc72ba6377d9b0557acf0ce2e3f3d752
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-02-24 19:18:03 +0200

    Setup config

---
+++ lab3/lab3/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace lab3;
+
+public record Log(string Body, int Level, DateTime CreatedAt)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
+
+public record LogArguments(string Body, int Level);
--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using lab3;
+using Nest;
+
+const string indexName = "logs";
+var client = new ElasticClient(new Uri("http://localhost:9200"));
+
+if (!(await client.Indices.ExistsAsync(indexName)).Exists)
+{
+    await client.Indices.CreateAsync(indexName, options =>
+    {
+        options.Map<Log>(m =>
+        {
+            m.Properties(p => p.Keyword(k => k.Name(log => log.Id)));
+            m.Properties(p => p.Text(k => k.Name(log => log.Body)));
+            m.Properties(p => p.IntegerRange(k => k.Name(log => log.Level)));
+            m.Properties(p => p.Date(k => k.Name(log => log.CreatedAt)));
+            return m;
+        });
+        
+        return options;
+    });
+}
