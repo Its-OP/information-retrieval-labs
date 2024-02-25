01afbce6f86482080fa1a84d240339629b6138c1
Oleh Prostakov <prostakov.oleh@gmail.com>
2024-02-25 20:12:20 +0200

    Debug the system

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,10 +1,14 @@
-﻿using lab3;
+﻿using System.Text.RegularExpressions;
+using lab3;
 using Nest;
 using Newtonsoft.Json;
 
 const string indexName = "logs";
 
-var client = new ElasticClient(new Uri("http://localhost:9200"));
+var settings = new ConnectionSettings(new Uri("http://localhost:9200"));
+settings.DisableDirectStreaming(false);
+
+var client = new ElasticClient(settings);
 
 if (!(await client.Indices.ExistsAsync(indexName)).Exists)
 {
@@ -12,10 +16,10 @@ if (!(await client.Indices.ExistsAsync(indexName)).Exists)
     {
         options.Map<Log>(m =>
         {
-            m.AutoMap<Log>();
+            // m.AutoMap<Log>();
             m.Properties(p => p.Keyword(k => k.Name(log => log.Source)));
             m.Properties(p => p.Text(k => k.Name(log => log.Body)));
-            m.Properties(p => p.IntegerRange(k => k.Name(log => log.Level)));
+            m.Properties(p => p.Number(k => k.Name(log => log.Level)));
             m.Properties(p => p.Date(k => k.Name(log => log.CreatedAt)));
             return m;
         });
@@ -27,14 +31,14 @@ if (!(await client.Indices.ExistsAsync(indexName)).Exists)
 while (true)
 {
     Console.WriteLine("Please enter a command:");
-    var command = Console.ReadLine() ?? string.Empty;
-    var commandParts = command.Split();
+    var command = Console.ReadLine();
+    var commandParts = MyRegex().Split(command ?? string.Empty).Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim('\'')).ToArray();
 
-    var response = (commandParts.First(), commandParts.Length) switch
+    var response = (commandParts.Length, commandParts.FirstOrDefault()) switch
     {
-        ("create", 2) => await ProcessCreateDocument(client, JsonConvert.DeserializeObject<LogArguments>(commandParts[1])),
-        ("get", >= 1 and <= 5) => await ProcessGetDocument(client, Guid.Parse(commandParts[1]), commandParts[1..]),
-        ("delete", 2) => await ProcessDeleteDocument(client, Guid.Parse(commandParts[1])),
+        (2, "create") => await ProcessCreateDocument(client, JsonConvert.DeserializeObject<LogArguments>(commandParts[1])),
+        (>= 1 and <= 5, "get") => await ProcessGetDocument(client, commandParts[1..]),
+        (2, "delete") => await ProcessDeleteDocument(client, Guid.Parse(commandParts[1])),
         _ => "Unknown command"
     };
     
@@ -52,57 +56,59 @@ static async Task<string> ProcessCreateDocument(IElasticClient client, LogArgume
     if (!response.IsValid)
         return "Failed to insert the new log";
 
-    return JsonConvert.SerializeObject(response);
+    return JsonConvert.SerializeObject(log);
 }
 
-static async Task<string> ProcessGetDocument(IElasticClient client, Guid logId, params string[] searchArguments)
+static async Task<string> ProcessGetDocument(IElasticClient client, params string[] searchArguments)
 {
-    if (logId == Guid.Empty)
-        return "Id is empty";
-
     var response = await client.SearchAsync<Log>(s => s.Index(indexName).Query(q =>
     {
+        var queries = new List<QueryContainer>();
+        
         var keywordParam = searchArguments.SingleOrDefault(x =>
-            x.StartsWith($"{nameof(Log.Source)}%exact", StringComparison.InvariantCultureIgnoreCase) && x.Contains(':'));
+            x.StartsWith($"{nameof(Log.Source)}%exact", StringComparison.InvariantCultureIgnoreCase) &&
+            x.Contains(':'));
         if (keywordParam is not null)
         {
             var paramValue = keywordParam.Split(':').Last();
-            q.Term(t => t.Field(log => log.Source).Value(paramValue));
+            queries.Add(q.Term(t => t.Field(log => log.Source).Value(paramValue)));
         }
-        
-        var rangeLessParam = searchArguments.SingleOrDefault(x =>
-            x.StartsWith(nameof(Log.Level), StringComparison.InvariantCultureIgnoreCase) && x.Contains(':') && x.Contains('<'));
-        if (rangeLessParam is not null)
-        {
-            var paramValue = rangeLessParam.Split('<').Last();
-            if (int.TryParse(paramValue, out var paramValueInt))
-                q.Range(t => t.Field(log => log.Source).LessThan(paramValueInt));
-        }
-        
-        var rangeGreaterParam = searchArguments.SingleOrDefault(x =>
-            x.StartsWith(nameof(Log.Level), StringComparison.InvariantCultureIgnoreCase) && x.Contains(':') && x.Contains('>'));
-        if (rangeGreaterParam is not null)
-        {
-            var paramValue = rangeGreaterParam.Split('>').Last();
-            if (int.TryParse(paramValue, out var paramValueInt))
-                q.Range(t => t.Field(log => log.Source).GreaterThan(paramValueInt));
-        }
-        
+
+        var rangeLessParam = searchArguments
+            .SingleOrDefault(x =>
+                x.StartsWith(nameof(Log.Level), StringComparison.InvariantCultureIgnoreCase) && x.Contains(':') &&
+                x.Contains('<'));
+        var rangeGreaterParam = searchArguments
+            .SingleOrDefault(x =>
+                x.StartsWith(nameof(Log.Level), StringComparison.InvariantCultureIgnoreCase) && x.Contains(':') &&
+                x.Contains('>'));
+
+        queries.Add(q.Range(t =>
+            t.Field(log => log.Level).LessThan(ExtractParamValue(rangeLessParam, '<', int.MaxValue))
+                .GreaterThan(ExtractParamValue(rangeGreaterParam, '>', int.MinValue))));
+
         var fuzzyParam = searchArguments.SingleOrDefault(x =>
-            x.StartsWith($"{nameof(Log.Source)}%fuzzy", StringComparison.InvariantCultureIgnoreCase) && x.Contains(':'));
+            x.StartsWith($"{nameof(Log.Source)}%fuzzy", StringComparison.InvariantCultureIgnoreCase) &&
+            x.Contains(':'));
         if (fuzzyParam is not null)
         {
             var paramValue = fuzzyParam.Split(':').Last();
-            q.Fuzzy(t => t.Field(log => log.Source).Value(paramValue));
+            queries.Add(q.Fuzzy(t => t.Field(log => log.Source).Value(paramValue).Fuzziness(Fuzziness.AutoLength(1, 5))));
         }
 
-        return q;
+        var finalQuery = q as QueryContainer;
+        foreach (var query in queries)
+        {
+            finalQuery = q && query;
+        }
+
+        return finalQuery;
     }));
     
     if (!response.IsValid)
         return "Failed to insert the new log";
-
-    return JsonConvert.SerializeObject(response);
+    
+    return JsonConvert.SerializeObject(response.Documents, Formatting.Indented);
 }
 
 static async Task<string> ProcessDeleteDocument(IElasticClient client, Guid logId)
@@ -115,5 +121,21 @@ static async Task<string> ProcessDeleteDocument(IElasticClient client, Guid logI
     if (!response.IsValid)
         return "Failed to insert the new log";
 
-    return JsonConvert.SerializeObject(response);
+    return "Deleted successfully";
+}
+
+static int ExtractParamValue(string? param, char separator, int defaultValue)
+{
+    var paramValue = param?.Split(separator).Last();
+    return int.TryParse(paramValue, out var intParamValue)
+        ? intParamValue
+        : defaultValue;
+}
+
+internal partial class Program
+{
+    [GeneratedRegex("""
+                    ('.*?'|\S+)
+                    """)]
+    private static partial Regex MyRegex();
 }
